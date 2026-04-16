3d4315ef896d37ea76116a930da7c7ad4ee4dca5
vinayrao098 <vinayrao098@gmail.com>
2026-04-16 19:56:11 +0530

    Clean initial commit - Portfolio website (no build artifacts)

--- .next/dev/static/chunks/app_page_tsx_0cuwn~l._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/_01l59ag._.js","static/chunks/node_modules_12yww4e._.js"],
-    source: "dynamic"
-});
